75dafd93ef6fe11c7c3dd9c1b08ffcc7d24bd399
Abhishek Verma <ak8888780@gmail.com>
2023-11-23 23:05:13 +0530

    get all collection

--- 26MongoDB-Setup-for-APT/main.go
+++ 26MongoDB-Setup-for-APT/main.go
@@ -2,6 +2,7 @@
 // go get go.mongodb.org/mongo-driver/mongo
 // login mongodb /sign in if you haven't
 // setup database deployment
+// ctrl +shift+p = go tools install and update select all
 package main
 
 import "fmt"
