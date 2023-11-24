59c4c108cee1f1d39ab9274e7663803f521916b9
Abhishek Verma <ak8888780@gmail.com>
2023-11-24 21:28:44 +0530

    wait groups in golang

--- 27concurrency-gorutines/main.go
+++ 27concurrency-gorutines/main.go
@@ -2,18 +2,45 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"net/http"
+	"sync"
 )
 
-func main() {
-	go greeter("hello")
-	greeter("abhii")
-}
+var wg sync.WaitGroup // pointers
 
-func greeter(s string) {
-	for i := 0; i < 5; i++ {
-		time.Sleep(3 * time.Millisecond)
-		fmt.Println(s)
+func main() {
+	// go greeter("hello")
+	// greeter("abhii")
+
+	websitelist := []string{
+		"https://lco.dev",
+		"https://go.dev",
+		"https://google.com",
+		"https://github.com",
+		"https://helloabhii.hashnode.dev",
 	}
 
+	for _, web := range websitelist {
+		go getStatusCode(web)
+		wg.Add(1)
+	}
+	wg.Wait()
+}
+
+// func greeter(s string) {
+// 	for i := 0; i < 5; i++ {
+// 		time.Sleep(3 * time.Millisecond)
+// 		fmt.Println(s)
+// 	}
+// }
+
+func getStatusCode(endpoint string) {
+	defer wg.Done()
+
+	res, err := http.Get(endpoint)
+	if err != nil {
+		fmt.Println("OOps in endpoint")
+	} else {
+		fmt.Printf("%d Status Code for %s\n", res.StatusCode, endpoint)
+	}
 }
