b5c08cdb8e2bafd05a552ee9e226453f39a9831f
Abhishek Verma <ak8888780@gmail.com>
2023-11-24 21:40:49 +0530

    mutex-lock

---
+++ 28mutex/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"sync"
+)
+
+var signals = []string{"test"}
+
+var wg sync.WaitGroup // pointers
+var mut sync.Mutex    // pointer
+
+func main() {
+	// go greeter("hello")
+	// greeter("abhii")
+
+	websitelist := []string{
+		"https://lco.dev",
+		"https://go.dev",
+		"https://google.com",
+		"https://github.com",
+		"https://helloabhii.hashnode.dev",
+	}
+
+	for _, web := range websitelist {
+		go getStatusCode(web)
+		wg.Add(1)
+	}
+	wg.Wait()
+	fmt.Println(signals)
+}
+
+// func greeter(s string) {
+// 	for i := 0; i < 5; i++ {
+// 		time.Sleep(3 * time.Millisecond)
+// 		fmt.Println(s)
+// 	}
+// }
+
+func getStatusCode(endpoint string) {
+	defer wg.Done()
+
+	res, err := http.Get(endpoint)
+	if err != nil {
+		fmt.Println("OOps in endpoint")
+	} else {
+		mut.Lock()
+		signals = append(signals, endpoint)
+		mut.Unlock()
+
+		fmt.Printf("%d Status Code for %s\n", res.StatusCode, endpoint)
+	}
+}
